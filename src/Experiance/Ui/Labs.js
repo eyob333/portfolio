14bf7209cdeaf44ef3d97b4ac10f92181a6b4b4f
eyob333 <jemijobs224@gmail.com>
2026-07-28 17:18:25 +0300

    fix: fix labs sections and minor ui update

--- src/Experiance/Ui/Labs.js
+++ src/Experiance/Ui/Labs.js
@@ -12,7 +12,6 @@ export default class Labs{
         let parent = document.createElement('section')
         parent.id = 'labs'
         parent.classList.add('page')
-        // parent.classList.add('labs')
         parent.innerHTML =  `
                 <div class="intro-header header-labs">
                     <div class="title-cont">  
@@ -31,11 +30,28 @@ export default class Labs{
     setInstance(){
         let element = document.querySelector('div.slider-lab .sliders');
         let i=0
-        let injectElement = this.array.map( d => `
-            <!-- ${i++} -->
-            <div class="slider-cont i-${i} ${i>3 ? 'tab-overflow':''}"> 
-                <div class="s-name">
-                    <h2>${d.name}</h2>
+        let injectElement = this.array.map( (d,j) => `
+            <div class="slider-cont i-${j}"> 
+                <div class="cont-frac"> 
+                    <span class="s-empty"> 
+                    </span>
+                    <div class="s-vis"> 
+                        <div class="s-image"> 
+                            <img src="${d.img}" >
+                        </div>
+                        <div class="s-icon"> 
+                            ${d.icon.map( k =>`
+                                <div class='icon-cont'> 
+                                    ${k.svg}
+                                </div>
+                                `)}
+                        </div> 
+                    </div>
+                    <div class="s-desc"> 
+                        <div class="s-name">
+                            <h2>${d.name}</h2>
+                        </div>
+                    </div>
                 </div>
             </div>`).join('');
         element.innerHTML = injectElement;
--- src/assets/labsCards.js
+++ src/assets/labsCards.js
@@ -1,53 +1,69 @@
+import icons from "./techStackIcons"
 
+
+
+const img = './images/thing.png'
+const icon = [
+    icons[0],
+    icons[2],
+    icons[4],
+    icons[6],
+
+]
 
 const slicedModel = {
-    img: 'https://res.cloudinary.com/dufjxw9zz/image/upload/f_webp/v1747926000/Screenshot_2025-05-22_174008_qqbg0f.webp',
     link: 'https://sliced-model-shaders.vercel.app/',
     name: 'sliced model',
     description: '',
-    role: 'ffsd'
+    role: 'ffsd',
+    img,
+    icon
 }
 
 const sortingVizualizer = {
-    img: 'https://res.cloudinary.com/dufjxw9zz/image/upload/f_webp/v1747926001/Screenshot_2025-05-22_173750_ihuj2l.webp',
     link: 'https://sort-visualizer-zeta.vercel.app/',
     name: 'sorting vizualizer',
     description: '',
     role: 'sdfsd',
     upTextC: '#000',
-    mainTextC: '#000'
+    mainTextC: '#000',
+    img,
+    icon
 }
 
 const particlesMorphing = {
-    img: 'https://res.cloudinary.com/dufjxw9zz/image/upload/f_webp/v1747926004/Screenshot_2025-05-22_174626_m9z1c1.webp',
     link: 'https://particles-morphing-shader-beige.vercel.app/',
     name: 'particle morphing',
     description: '',
-    role: 'sdfse'
+    role: 'sdfse',
+    img,
+    icon
 }
 
 const particlescursor = {
-    img: '/images/Screenshot 2025-05-18 141904.png',
     link: 'https://particle-cursor-shaders.vercel.app/',
     name: 'particles cursor',
     description: '',
-    role: 'sefsfe'
+    role: 'sefsfe',
+    img,
+    icon
 }
 
 const earthShader =  {
-    img: 'https://res.cloudinary.com/dufjxw9zz/image/upload/f_webp/v1747926000/Screenshot_2025-05-22_174133_hxgeci.webp',
     link: 'https://earth-shaders-b4.vercel.app/',
     name: 'earth shader',
     description: '',
-    role: 'sfseft'
+    role: 'sfseft',
+    img,
+    icon
 }
 
 const LabsCards = [
     slicedModel,
     sortingVizualizer,
     particlesMorphing,
-    // particlescursor,
-    // earthShader
+    particlescursor,
+    earthShader
 ]
 
 export default LabsCards
